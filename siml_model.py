bf7d1e6b04184f3e17caa8c75b5c31043b758c28
Maciej-GBS <12584491+Maciej-GBS@users.noreply.github.com>
2024-05-12 17:09:42 +0200

    optimized model slightly

--- siml_model.py
+++ siml_model.py
@@ -4,20 +4,17 @@ def build_model(num_days, data_size) -> tf.keras.models.Model:
     model = tf.keras.models.Sequential()
 
     model.add(tf.keras.layers.Input(shape=[num_days, data_size]))
-    model.add(tf.keras.layers.LSTM(units=77, activation='relu', return_sequences=True))
-    model.add(tf.keras.layers.Dropout(rate=0.3))
-    model.add(tf.keras.layers.LSTM(units=37, activation='relu', return_sequences=True))
-    model.add(tf.keras.layers.Dropout(rate=0.3))
 
     model.add(tf.keras.layers.LSTM(units=24, activation='relu', return_sequences=True))
-    model.add(tf.keras.layers.Dropout(rate=0.2))
+    model.add(tf.keras.layers.Dropout(rate=0.3))
     model.add(tf.keras.layers.LSTM(units=12, activation='relu', return_sequences=True))
-    model.add(tf.keras.layers.Dropout(rate=0.2))
+    model.add(tf.keras.layers.Dropout(rate=0.3))
 
     model.add(tf.keras.layers.LSTM(units=6, activation='relu', return_sequences=True))
     model.add(tf.keras.layers.Dropout(rate=0.2))
     model.add(tf.keras.layers.Flatten())
-    model.add(tf.keras.layers.BatchNormalization())
+    #model.add(tf.keras.layers.BatchNormalization())
+    model.add(tf.keras.layers.Dense(units=10, activation='relu'))
     # for regression use linear activation, i.e. no activation function is needed
     # sigmoid and other min/max value limiting functions should be avoided
     model.add(tf.keras.layers.Dense(units=1, activation='linear'))
