333b38833cff189e4acfd87b16ca26e660517486
Maciej-GBS <12584491+Maciej-GBS@users.noreply.github.com>
2024-05-12 16:56:31 +0200

    added lab3

--- siml_pipeline.py
+++ siml_pipeline.py
@@ -24,13 +24,19 @@ class Pipeline:
             self.scaler_.fit_transform(X.iloc[:split_idx]), self.num_days, axis=0).swapaxes(1,2)
         X_test = sliding_window(
             self.scaler_.transform(X.iloc[split_idx:]), self.num_days, axis=0).swapaxes(1,2)
+        # remove last element from X as we want to predict next Y value
+        X_train = X_train[:-1]
+        X_test = X_test[:-1]
 
         # skip first elements as these cannot be predicted due to lack of input data
         Y_train = self.y_scaler_.fit_transform(Y.iloc[:split_idx].to_numpy().reshape(-1, 1))[self.num_days:]
         Y_test = self.y_scaler_.transform(Y.iloc[split_idx:].to_numpy().reshape(-1, 1))[self.num_days:]
 
-        # remove last element from X as we want to predict next Y value
-        return X_train[:-1], X_test[:-1], Y_train, Y_test
+        # shuffle
+        shuffled_indexes = np.arange(Y_train.shape[0])
+        np.random.shuffle(shuffled_indexes)
+
+        return X_train[shuffled_indexes], X_test, Y_train[shuffled_indexes], Y_test
 
     def process_test(self, test_data: pd.DataFrame):
         sliding_window = np.lib.stride_tricks.sliding_window_view
