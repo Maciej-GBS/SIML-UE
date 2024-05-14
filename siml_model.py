da75e843c1ab25917003d71f95c10f60ec54435f
Maciej-GBS <12584491+Maciej-GBS@users.noreply.github.com>
2024-05-14 09:49:00 +0200

    added single feature model training

--- siml_model.py
+++ siml_model.py
@@ -1,6 +1,6 @@
 import tensorflow as tf
 
-def build_model(num_days, data_size) -> tf.keras.models.Model:
+def build_model(num_days, data_size, output_size=1) -> tf.keras.models.Model:
     model = tf.keras.models.Sequential()
 
     model.add(tf.keras.layers.Input(shape=[num_days, data_size]))
@@ -13,10 +13,10 @@ def build_model(num_days, data_size) -> tf.keras.models.Model:
     model.add(tf.keras.layers.LSTM(units=6, activation='relu', return_sequences=True))
     model.add(tf.keras.layers.Dropout(rate=0.2))
     model.add(tf.keras.layers.Flatten())
-    #model.add(tf.keras.layers.BatchNormalization())
+    #model.add(tf.keras.layers.BatchNormalization()) # reduces quality
     model.add(tf.keras.layers.Dense(units=10, activation='relu'))
     # for regression use linear activation, i.e. no activation function is needed
     # sigmoid and other min/max value limiting functions should be avoided
-    model.add(tf.keras.layers.Dense(units=1, activation='linear'))
+    model.add(tf.keras.layers.Dense(units=output_size, activation='linear'))
 
     return model
--- siml_pipeline.py
+++ siml_pipeline.py
@@ -1,13 +1,23 @@
 import numpy as np
 import pandas as pd
 from sklearn.preprocessing import StandardScaler
-#from sklearn.preprocessing import MinMaxScaler
 
 class Pipeline:
     def __init__(self, num_days=10):
         self.num_days = num_days
         self.scaler_ = None
         self.y_scaler_ = None
+
+    @staticmethod
+    def _sliding_window(array, window_size: int):
+        # sliding window adds new axis at index 2 but it should be at 1
+        return np.lib.stride_tricks.sliding_window_view(array, window_size, axis=0).swapaxes(1, 2)
+
+    @staticmethod
+    def _shuffled_index(array):
+        idx = np.arange(array.shape[0])
+        np.random.shuffle(idx)
+        return idx
 
     def process(self, data: pd.DataFrame, y_label, split=0.75):
         split_idx = int(split * len(data))
@@ -18,12 +28,8 @@ class Pipeline:
         X = data.drop(columns=[y_label])
         Y = data[y_label]
 
-        sliding_window = np.lib.stride_tricks.sliding_window_view
-        # sliding window adds new axis at index 2 but it should be at 1
-        X_train = sliding_window(
-            self.scaler_.fit_transform(X.iloc[:split_idx]), self.num_days, axis=0).swapaxes(1,2)
-        X_test = sliding_window(
-            self.scaler_.transform(X.iloc[split_idx:]), self.num_days, axis=0).swapaxes(1,2)
+        X_train = self._sliding_window(self.scaler_.fit_transform(X.iloc[:split_idx]), self.num_days)
+        X_test = self._sliding_window(self.scaler_.transform(X.iloc[split_idx:]), self.num_days)
         # remove last element from X as we want to predict next Y value
         X_train = X_train[:-1]
         X_test = X_test[:-1]
@@ -32,17 +38,38 @@ class Pipeline:
         Y_train = self.y_scaler_.fit_transform(Y.iloc[:split_idx].to_numpy().reshape(-1, 1))[self.num_days:]
         Y_test = self.y_scaler_.transform(Y.iloc[split_idx:].to_numpy().reshape(-1, 1))[self.num_days:]
 
-        # shuffle
-        shuffled_indexes = np.arange(Y_train.shape[0])
-        np.random.shuffle(shuffled_indexes)
-
+        shuffled_indexes = self._shuffled_index(Y_train)
         return X_train[shuffled_indexes], X_test, Y_train[shuffled_indexes], Y_test
 
     def process_test(self, test_data: pd.DataFrame):
-        sliding_window = np.lib.stride_tricks.sliding_window_view
-        X = sliding_window(
-            self.scaler_.fit_transform(test_data), self.num_days, axis=0).swapaxes(1,2)
+        X = self._sliding_window(self.scaler_.transform(test_data), self.num_days)
         return X
 
     def restore(self, output):
         return self.y_scaler_.inverse_transform(output)
+
+class SingleFeaturePipeline(Pipeline):
+    def __init__(self, num_days=10, look_ahead=8):
+        super().__init__(num_days)
+        self.look_ahead = look_ahead
+
+    def process(self, y_series, split=0.75):
+        split_idx = int(split * len(y_series))
+
+        self.scaler_ = StandardScaler()
+        self.y_scaler_ = self.scaler_
+
+        assert self.num_days >= self.look_ahead, "Look ahead cannot be larger than number look back of days"
+        X = self._sliding_window(
+            self.scaler_.fit_transform(y_series[:-self.look_ahead].values.reshape(-1, 1)), self.num_days)
+        # truncate Y sliding window data because a smaller window will always produce more samples than needed
+        Y = self._sliding_window(
+            self.scaler_.transform(y_series[self.look_ahead:].values.reshape(-1, 1)), self.look_ahead)[:X.shape[0]]
+
+        X_train = X[:split_idx]
+        X_test = X[split_idx:]
+        Y_train = Y[:split_idx]
+        Y_test = Y[split_idx:]
+
+        shuffled_idx = self._shuffled_index(Y_train)
+        return X_train[shuffled_idx], X_test, Y_train[shuffled_idx], Y_test
